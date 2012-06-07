a19334aa149e790f0c6c3cd278e82fbcd20b611c
Olof Johansson <olof@ethup.se>
2012-06-08 00:03:53 +0200

    Initial commit

---
+++ bin/svtplay.pl
@@ -0,0 +1,119 @@
+#!/usr/bin/perl
+# Copyright 2011, 2012, Olof Johansson <olof@ethup.se>
+# Copyright 2011, Magnus Woldrich <magnus@trapd00r.se>
+#
+# Copying and distribution of this file, with or without
+# modification, are permitted in any medium without royalty
+# provided the copyright notice are preserved. This file is
+# offered as-is, without any warranty.
+
+use vars qw($VERSION);
+$VERSION = '0.3';
+my $APP  = 'svtplay';
+
+use strict;
+use warnings;
+use feature qw/say/;
+
+use WWW::SVT::Play;
+use Getopt::Long qw/:config gnu_getopt/;
+use Pod::Usage qw/pod2usage/;
+
+sub usage {
+	pod2usage(
+		verbose  => 99,
+		exitval  => 0,
+		sections => q{DESCRIPTION|OPTIONS},
+	);
+}
+
+usage() unless(@ARGV);
+
+my $opts = {
+	bitrate => 0,
+	help => \&usage,
+	version => sub { say("$APP v", __PACKAGE__->VERSION) && exit },
+};
+GetOptions($opts,
+	'bitrate|b:i',
+	'max-bitrate|m',
+	'download|d',
+	'output|o=s',
+	'help|h',
+	'version|v',
+);
+
+my $url = shift;
+my $svtp = WWW::SVT::Play->new($url) or
+	die "Failed?";
+my $bitrate = $opts->{bitrate};
+$bitrate = $svtp->bitrates if $opts->{'max-bitrate'};
+
+if($bitrate) {
+	if($opts->{download}) {
+		download($svtp, $bitrate);
+	} else {
+		say $svtp->url($bitrate);
+	}
+} else {
+	say "W: You have to do specify a bitrate" if $opts->{download};
+	say "$_: ", $svtp->url($_) for $svtp->bitrates;
+}
+
+exit 0;
+
+sub download {
+	my $svtp = shift;
+	my $bitrate = shift;
+
+	my $url = $svtp->url($bitrate);
+	my $filename = $svtp->filename($bitrate);
+	print "using filename $filename\n\n";
+	exec("rtmpdump -r $url -o $filename");
+}
+
+__END__
+
+=pod
+
+=head1 NAME
+
+svtplay - extract RTMP URLs from svtplay.se
+
+=head1 DESCRIPTION
+
+svtplay is a script that lets you extract RTMP URLs from SVT Play.
+You can feed this URL to e.g. rtmpdump and extract the video using
+options to the script.
+
+=head1 SYNOPSIS
+
+  svtplay [OPTIONS] <URL>
+
+=head1 OPTIONS
+
+  -d, --download     download video
+  -b, --bitrate      choose bitrate. only list available bitrates if omitted
+  -m, --max-bitrate  choose the best available bitrate
+  -o, --output file  specify output filename
+
+  -h, --help         show the help and exit
+  -v, --version      show version info and exit
+
+=head1 CONTRIBUTORS
+
+=over
+
+=item Magnus Woldrich, magnus@trapd00r.se, http://japh.se, trapd00r on github
+
+=back
+
+=head1 COPYRIGHT
+
+Copyright 2011, Olof Johansson <olof@ethup.se>
+(and contributors...)
+
+Copying and distribution of this file, with or without
+modification, are permitted in any medium without royalty
+provided the copyright notice are preserved. This file is
+offered as-is, without any warranty.
---
+++ lib/WWW/SVT/Play.pm
@@ -0,0 +1,215 @@
+package WWW::SVT::Play;
+
+# Copyright (c) 2012 - Olof Johansson <olof@cpan.org>
+# All rights reserved.
+# 
+# This program is free software; you can redistribute it and/or 
+# modify it under the same terms as Perl itself.
+
+=head1 NAME
+
+WWW::SVT::Play, extract information about videos on SVT Play
+
+=head1 SYNOPSIS
+
+ my $uri = 'http://www.svtplay.se/video/107889/malmopolisen-del-6';
+ my $svtp = WWW::SVT::Play->new($uri);
+ say $svtp->title;
+
+ my $max = 0;
+ $max = $max < $_ ? $_ : $max for $svtp->bitrates;
+ my $stream_uri = $svtp->uri($max);
+ my $filename = $svtp->filename($max);
+ 
+=head1 DESCRIPTION
+
+Swedish state public service television, Sveriges Television, has
+created a popular "Play" service (i.e. on demand web based
+streaming). But one thing was missing. An API! So we have to
+scrape unforuntately.
+
+=cut
+
+our $VERSION = 0.1;
+use HTML::TreeBuilder;
+use LWP::UserAgent;
+use List::Util qw/max/;
+use Encode;
+use JSON;
+
+=head1 CONSTRUCTOR
+
+=head2 WWW::SVT::Play->new($uri)
+
+Construct a WWW::SVT::Play object by passing the URL to the video
+you're interested in. A second argument consisting of a hashref
+of options is reserved for future use.
+
+=cut
+
+sub new {
+	my $class = shift;
+	my $uri = shift;
+
+	$uri .= '?type=embed' unless $uri =~ /\?/;
+	$uri .= '&type=embed' unless $uri =~ /[\&\?]type=/;
+	my $html = _get($uri);
+	my $json = _extract_json($html);
+	
+	my %bitrate = map {
+		$_->{bitrate} => $_->{url}
+	} @{$json->{video}->{videoReferences}};
+	
+	my @subtitles = map {
+		$_->{url}
+	} @{$json->{video}->{subtitleReferences}};
+
+	bless {
+		url => $uri,
+		streams => \%bitrate,
+		filename => $json->{statistics}->{title},
+		subtitles => \@subtitles,
+		title => $json->{context}->{title},
+	}, $class;
+}
+
+=head2 url
+
+Returns the URL, originally supplied to the constructor
+after it has been postprocessed somewhat.
+
+=cut
+
+sub url {
+	my $self = shift;
+	my $bitrate = shift;
+
+	return $self->{streams}->{$bitrate};
+}
+
+=head2 title
+
+Returns a human readable title for the video.
+
+=cut
+
+sub title {
+	my $self = shift;
+	return $self->{title};
+}
+
+=head2 $svtp->filename($bitrate)
+
+Returns a filename suggestion for the video. If you give the
+optional bitrate argument, you also get a file extension.
+
+=cut
+
+sub filename {
+	my $self = shift;
+	my $bitrate = shift;
+	return $self->{filename} unless $bitrate;
+	return $self->{filename} . '.' . $self->format($bitrate);
+}
+
+=head2 $svtp->bitrates
+
+In list context, returns a list of available bitrates for the
+video. In scalar context, the highest available bitrate is
+returned. In either case, the returned values can be used as
+argument to the module whenever a bitrate is asked for.
+
+=cut
+
+sub bitrates {
+	my $self = shift;
+	my @streams;
+	@streams = keys %{$self->{streams}} if $self->{streams};
+	return @streams if wantarray;
+	return max(@streams);
+}
+
+=head2 $svtp->format($bitrate)
+
+Returns a "guess" of what the format is, by trying to extract a
+file extension from the stream URL. Of course, the format depends
+on what bitrate you want, so you have to supply that.
+
+=cut
+
+sub format {
+	my $self = shift;
+	my $bitrate = shift;
+
+	my ($ext) = $self->{streams}->{$bitrate} =~ m#\.([^/]+?)$#;
+	return $ext;
+}
+
+=head2 $svtp->subtitles
+
+In list context, returns a list of URLs to subtitles. In scalar
+context, returns the first URL in that list. If there are no
+subtitles available for this video, returns an empty list (in
+list context) or undef (in scalar context).
+
+=cut
+
+sub subtitles {
+	my $self = shift;
+	my @subtitles;
+	push @subtitles, @{$self->{subtitles}} if $self->{subtitles};
+	return @subtitles if wantarray;
+	return $subtitles[0];
+}
+
+## INTERNAL SUBROUTINES
+##  These are *not* easter eggs or something like that. Yes, I'm
+##  looking at you, Woldrich!
+
+sub _get {
+	my $uri = shift;
+	my $ua = LWP::UserAgent->new(
+		agent => "WWW::SVT::Play/$VERSION",
+	);
+	$ua->env_proxy;
+	my $resp = $ua->get($uri);
+	
+	return $resp->decoded_content if $resp->is_success;
+	die "Failed to fetch $uri: $response->status_line";
+}
+
+sub _extract_json {
+	my $html = shift;
+	my $tree = HTML::TreeBuilder->new_from_content($html);
+	my $param = $tree->look_down(
+		_tag => 'param',
+		name => 'flashvars',
+	);
+
+	if($param and my($json) = $param->attr('value') =~ /^json=(.*)/) {
+		# SVT claims it's utf-8, but it's not... not the json. at
+		# least in one case it was iso-8859-15.
+		my $jsonenc = encode('utf-8', decode('iso-8859-15', $json));
+		return decode_json($jsonenc);
+	}
+
+	die "Could not find needed parameters from SVT Play";
+}
+
+=head1 AVAILABILITY AND BUG REPORTING
+
+The latest released version of this module is available through
+CPAN. It's encouraged to report bugs on github.com, but I will
+also monitor the CPAN RT and (of course,) e-mail.
+
+=head1 COPYRIGHT
+
+Copyright (c) 2012 - Olof Johansson <olof@cpan.org>
+All rights reserved.
+
+This program is free software; you can redistribute it and/or 
+modify it under the same terms as Perl itself.
+
+=cut
+
+1;
